7b1d3ca3292fdb5cdd9929f4f73cc4470c6f4a81
sawermager <wermager3004@gmail.com>
2020-04-22 09:33:57 -0500

    README cleanup

---
+++ qemu-initramfs-only/gg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -d ./mount.point.dir ]
+then
+	echo "found dir"
+else
+	echo "no dir"
+fi
--- qemu-initramfs-only/qemu-conf.sh
+++ qemu-initramfs-only/qemu-conf.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
-
-# BOOT QEMU VM WITH ANY DEBIAN RELEASE
-# https://www.collabora.com/news-and-blog/blog/2017/01/16/setting-up-qemu-kvm-for-kernel-development/
-
-# QEMU setup script to include a rootfs
 IMG=qemu-image.img
 DIR=mount-point.dir
-qemu-img create $IMG 1g
-mkfs.ext2 $IMG
-mkdir $DIR
-sudo mount -o loop $IMG $DIR
-# Using buster here as it is the current debian stable release (4/16/2020)
-sudo debootstrap --arch amd64 buster $DIR
-sudo umount $DIR
-rmdir $DIR
+
+function create_image() {
+    qemu-img create $IMG 1g
+    mkfs.ext4 $IMG
+    mkdir $DIR
+    sudo mount -o loop $IMG $DIR
+    # Using buster here as it is the current debian stable release (4/16/2020)
+    sudo debootstrap --arch amd64 buster $DIR
+    sudo umount $DIR
+    rmdir $DIR
+}
+
+
+echo "Enter kernel version to boot: "
+ls /boot/vm*
+read -p "Enter kernel version to boot: " kernel_ver
+if [ -f qemu-img ]
+then
+	read -p "Overwrite existing image?(y|n): " reply
+	[[ $reply == "y" ]] && create_image
+else 
+	create_image
+fi
+
+kernel="/boot/vmlinuz-${kernel_ver}"
+sudo qemu-system-x86_64 -kernel $kernel\
+                          -hda qemu-image.img\
+                          -append "root=/dev/sda single"\
+			  --enable-kvm\
+			  -nographic\
+			  -append "console=ttyS0 root=/dev/sda single"
